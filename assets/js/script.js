fdab2216b72dedb6d2b4ccd712e3b323591964a6
Alex C <alex.christianson.05@gmail.com>
2021-09-17 12:12:57 -0500

    created starting point for checkAnswer function

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,6 +19,7 @@ var timerState;
 var initialTime = 60;
 var questionContainer = document.querySelector("#questions");
 var questionIndex = 0;
+var feedback = document.querySelector("#answer-feedback");
 // start function to start timer, hide start screen, function to grab questions and answers array and display
 function quizStart () {
     startScreen.setAttribute("class", "hide");
@@ -30,22 +31,42 @@ function quizStart () {
 
 askQuestion();
 };
+
 // function to check if selected answer is correct or incorrect and add time penalty if incorrect
 function askQuestion () {
     var displayedQuestion = questions[questionIndex];
     var questionTitle = document.querySelector("#question-title");
     var questionText = document.querySelector("#question-text");
     questionTitle.textContent = displayedQuestion.title;
-
+    questionText.innerHTML = "";
     displayedQuestion.choices.forEach(function(choice) {
         var createButton = document.createElement("button");
         createButton.setAttribute("class", "choice");
         createButton.setAttribute("value", choice);
         createButton.textContent = choice;
+        createButton.onclick = checkAnswer;
         questionText.appendChild(createButton);
     })
-}
+};
 
+function checkAnswer () {
+    if (this.value === questions[questionIndex].answer) {
+        feedback.textContent = "Correct!"
+    }
+    else {
+        feedback.textContent = "Wrong!"
+        initialTime = initialTime - 10;
+        timerDisplay.textContent = initialTime;
+    }
+
+    questionIndex++;
+    if (questionIndex === questions.length) {
+        // quiz end function here (clearInterval on timerState)
+    }
+    else {
+    askQuestion();
+    }
+};
 
 // quiz end function to stop when questions run out or timer runs out (clearInterval on timer) and display endscreen with score
 
