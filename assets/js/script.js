d896078a481c37ed3889f89c6d5642c3c966eae5
Alex C <alex.christianson.05@gmail.com>
2021-09-17 11:47:35 -0500

    add script to start quiz and display question

---
+++ assets/js/script.js
@@ -0,0 +1,56 @@
+// create array for questions and answers
+var questions = [
+    {
+     title: "Commonly used data types DO NOT include:",
+     choices: ["strings", "booleans", "alerts", "numbers"],
+     answer: "alerts"
+    },
+    {
+     title: "The condition in an if / else statement is enclosed within ____.",
+     choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+     answer: "parentheses"
+    }
+   ]
+// create global variables that point to different html elements
+var startButton = document.querySelector("#start-button");
+var startScreen = document.querySelector("#start-page");
+var timerDisplay = document.querySelector("#timer");
+var timerState;
+var initialTime = 60;
+var questionContainer = document.querySelector("#questions");
+var questionIndex = 0;
+// start function to start timer, hide start screen, function to grab questions and answers array and display
+function quizStart () {
+    startScreen.setAttribute("class", "hide");
+    questionContainer.removeAttribute("class");
+    timerState = setInterval(function() {
+        initialTime--
+        timerDisplay.textContent = initialTime;
+    }, 1000) 
+
+askQuestion();
+};
+// function to check if selected answer is correct or incorrect and add time penalty if incorrect
+function askQuestion () {
+    var displayedQuestion = questions[questionIndex];
+    var questionTitle = document.querySelector("#question-title");
+    var questionText = document.querySelector("#question-text");
+    questionTitle.textContent = displayedQuestion.title;
+
+    displayedQuestion.choices.forEach(function(choice) {
+        var createButton = document.createElement("button");
+        createButton.setAttribute("class", "choice");
+        createButton.setAttribute("value", choice);
+        createButton.textContent = choice;
+        questionText.appendChild(createButton);
+    })
+}
+
+
+// quiz end function to stop when questions run out or timer runs out (clearInterval on timer) and display endscreen with score
+
+// save score and initials function into localStorage as object
+
+// get high score and initials when "view high scores" is clicked
+
+startButton.onclick = quizStart;
